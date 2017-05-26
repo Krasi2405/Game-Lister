1679cb126d1e8a813cc6c71a5f6b67af1ca2aee2
Krasimir Georgiev <krasi2405@gmail.com>
2017-05-27 00:11:00 +0300

    Fixed encoding bug, reworked upcoming games list by getting the games that have a release date that is later than today instead of getting all release dates then getting the games by release date game id. Tremendiously improves speed.

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ def search_games(search_phrase, limit = 10):
 	address = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit={}&offset=0&order=release_dates.date%3Adesc&search={}".format(limit, search_phrase)
 	return get_info_as_json(address)
 
+
+os.system("chcp 65001") # Set the encoding to utf-8
 clear()
 display_help()
 while(True):
@@ -59,13 +61,15 @@ while(True):
 				print("--------------------------------------------\n")
 	elif cmd == "upcoming":
 		curr_time = 1495753635000 # 5/26/2017 In Unix time in miliseconds
-		release_dates = get_info_as_json("https://igdbcom-internet-game-database-v1.p.mashape.com/release_dates/?fields=*&filter[date][gte]={}&limit=50&order=date:asc".format(int(curr_time)))
-		for release_date in release_dates:
-			game_name = get_info_as_json("https://igdbcom-internet-game-database-v1.p.mashape.com/games/{}?fields=name".format(release_date['game']))
+		search = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,release_dates&limit=50&filter[first_release_date][gte]={}&order=first_release_date:asc".format(curr_time)
+		upcoming_games = get_info_as_json(search)
+		for game in upcoming_games:
 			try:
-				print("Release Date: {} - Unix Timestamp: {} - Game Name: {}\n\n".format(release_date['human'], release_date['date'], game_name[0]['name']))
-			except:
-				print("Couldnt print information for game with id: {}".format(release_date['game']))
+				print(u"Time: {} - Unix Timestamp: {} - Game Name: {}".format(
+					game['release_dates'][0]['human'], game['release_dates'][0]['date'], game['name']))
+			except TypeError:
+				print("Couldnt print information for game with id: {}".format(game['id']))
+				print(u"Printing raw info for game: \n{}".format(game))
 	elif cmd == "id":
 		id = int(input("Enter the id of the game: "))
 		print(get_info_as_json("https://igdbcom-internet-game-database-v1.p.mashape.com/games/{}?fields=name".format(id)))
